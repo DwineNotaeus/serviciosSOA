33bee9326019fad1fd55b4e63d4d27f276e23cbc
Edwin Villa <dwine29@gmail.com>
2018-12-07 17:53:18 -0500

    Implementando Servicios Web

---
+++ ServiciosWeb.ServicioWCF/Service1.svc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using ServiciosWeb.Datos.Modelo;
+
+namespace ServiciosWeb.ServicioWCF
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Service1 : IService1
+    {
+        readonly LibreriaEntities BD = new LibreriaEntities();
+        public List<Libro> ObtenerLibros()
+        {
+            return BD.Libro.ToList();
+        }
+    }
+}
---
+++ ServiciosWeb.WebApi/Controllers/LibroController.cs
@@ -0,0 +1,31 @@
+﻿using ServiciosWeb.Datos.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ServiciosWeb.WebApi.Controllers
+{
+    public class LibroController : ApiController
+    {
+        readonly LibreriaEntities BD = new LibreriaEntities();
+        /// <summary>
+        /// Permite consultar la información de todos los libros
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<Libro> Get()
+        {
+            var listado = BD.Libro.ToList();
+            return listado;
+        }
+        [HttpPost]
+        public Libro Get(int ID)
+        {
+            var libro = BD.Libro.FirstOrDefault(x => x.Id_Libro == ID);
+            return libro;
+        }
+    }
+}
